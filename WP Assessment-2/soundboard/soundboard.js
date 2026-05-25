bd8da91fb2b9a6fef1d68e4fb93fbd4e5961acca
snowyyfare8 <snowytejada08@gmail.com>
2026-05-25 17:47:47 +0300

    Add files via upload

---
+++ WP Assessment-2/soundboard/soundboard.js
@@ -0,0 +1,53 @@
+// Plays ah-ha sfx
+function playSound1(){
+    var music = new Audio('ah-ha.mp3');
+    music.play();
+}
+
+// Plays Back of the net sfx
+function playSound2(){
+    var music = new Audio('back-of-the-net.mp3');
+    music.play();
+}
+
+// Plays Bang out of order sfx
+function playSound3(){
+    var music = new Audio('bangoutoforder.mp3');
+    music.play();
+}
+
+// Plays Dan sfx
+function playSound4(){
+    var music = new Audio('dan.mp3');
+    music.play();
+}
+
+// Plays Email of the evening sfx
+function playSound5(){
+    var music = new Audio('emailoftheevening.mp3');
+    music.play();
+}
+
+// Plays Hello partridge sfx
+function playSound6(){
+    var music = new Audio('hellopartridge.mp3');
+    music.play();
+}
+
+// Plays I ate a scotch egg sfx
+function playSound7(){
+    var music = new Audio('iateascotchegg.mp3');
+    music.play();
+}
+
+// Plays Im confused sfx
+function playSound8(){
+    var music = new Audio('kirk.mp3');
+    music.play();
+}
+
+// Plays ayyyyy sfx
+function playSound9(){
+    var music = new Audio('ayyy-ayyy-ayyy-scream.mp3');
+    music.play();
+}
